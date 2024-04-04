f87ca76460243d5203c57f328bde6538171113ae
GitUser6910 <idinahuiti890@gmail.com>
2024-04-04 23:01:59 +0200

    update

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,15 @@
-import java.time.DayOfWeek;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String[] monday = {"699 873 5734 | 2bQ1W9", "252 582 6634 | uE7h8Y", "576 979 1873 | xq9FNr", "576 979 1873 | xq9FNr"};
-        String[] mondayLessons = {"Биология", "Захист Украины", "Физика", "Физика"};
+        String[] mondayLessons = {"Биология", "Защита Украины", "Физика", "Физика"};
         String[] tuesday = {"852 361 9153 | 4uLinX", "873 252 9989 | XYCbr9", "523  403 2616 | 2SteFq", "705 855 4520 | 3VUgCE"};
         String[] tuesdayLessons = {"История Украины", "Физ-ра", "Зарубежная Литература", "Украинская Литература"};
         String[] wednesday = {"907 846 1000 | BYuFZ7", "428 666 0817 | SAxch8", "730 006 6521 | Uhjn1W", "929 781 1306 | без кода"};
         String[] wednesdayLessons = {"География", "Информатика", "Математика", "Английский Язык"};
         String[] thursday = {"523  403 2616 | 2SteFq", "210 923 1684 | 6GNiHA", "705 855 4520 | 3VUgCE", "705 855 4520 | 3VUgCE", "428 666 0817 | SAxch8"};
-        String[] thursdayLessons = {"География", "Информатика", "Математика", "Английский Язык"};
+        String[] thursdayLessons = {"Зарубежная Литература", "Всемировая История", "Украинский Язык", "Украинская Литература", "Информатика"};
         String[] friday = {"929 781 1306 | без кода", "210 923 1684 | 6GNiHA", "699 873 5734 | 2bQ1W9", "907 846 1000 | BYuFZ7", "766 4394 3770 | 707965"};
         String[] fridayLessons = {"Английский Язык", "Всемировая История", "Биология", "География", "Химия"};
         Scanner scanner = new Scanner(System.in);
